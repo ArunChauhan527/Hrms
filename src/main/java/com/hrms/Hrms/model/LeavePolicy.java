e0c246a7dcbda120609fa1a422e0f7ad2a87cad0
arun.chauhan <arun.chauhan1098@gmail.com>
2022-02-15 13:24:55 +0530

    Lombook commit
    Change-Id: Ib7b40e7c4ab51a3e4889665c7a35c84d31b3b4b7
    Signed-off-by: arun.chauhan <arun.chauhan1098@gmail.com>

--- src/main/java/com/hrms/Hrms/model/LeavePolicy.java
+++ src/main/java/com/hrms/Hrms/model/LeavePolicy.java
@@ -11,20 +11,22 @@ import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name="admin")
+@Table( name = "leavePloicy")
 @Data
-public class Admin {
+public class LeavePolicy {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	int sno;
-	String role;
-	String access_module;
 	String created_by;
 	String updated_by;
 	Date createdat;
 	Date updatedat;
 	String company;
+	float paid_leave;
+	float casual_leave; 
+	String casual_carry_fd;
+	String paid_carry_fd;
 	String industry;
 	
 }
